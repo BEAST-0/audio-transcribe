3e7d76129388a200989192d9b73e40e108cfb5cb
Anu J Pillai <anujpillai@revyrieglobal.com>
2025-02-25 16:06:58 +0530

    code optimized

--- myproject/speech/models.py
+++ myproject/speech/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
 class Meeting(models.Model):
     id = models.AutoField(primary_key=True)
     userid = models.IntegerField()
@@ -19,12 +21,22 @@ class MeetingTranscription(models.Model):
     def __str__(self):
         return f"{self.speaker} - {self.meeting.title}"
 
-class CustomUser(models.Model):  # Change class name
+class CustomUser(models.Model):  
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     code = models.CharField(max_length=10, unique=True)
+    password = models.CharField(
+        max_length=255,
+        validators=[
+            RegexValidator(
+                regex=r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+                message="Password must be at least 8 characters long, contain at least one letter, one number, and one special character."
+            )
+        ]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
